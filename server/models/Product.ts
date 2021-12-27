903ad3202d5e071f85e22f124fdb4f775e9fa17a
lemon14454 <lemon14454@gmail.com>
2021-12-27 15:59:29 +0800

    MongoDB unique Image value fixed

--- server/models/Product.ts
+++ server/models/Product.ts
@@ -4,7 +4,7 @@ const ProductSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
     name: { type: String, required: true },
-    image: { type: String, required: true, unique: true },
+    image: { type: String, required: true },
     brand: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
